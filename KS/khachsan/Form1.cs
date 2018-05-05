881ec7c2f5569dc40a7791c13aa160ed06a57a91
LinhNguyensl <35495662+LinhNguyensl@users.noreply.github.com>
2018-05-05 20:10:17 +0700

    update form Nhan viên

---
+++ KS/khachsan/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace khachsan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'hotelDataSet1.NhanVien' table. You can move, or remove it, as needed.
+            this.nhanVienTableAdapter.Fill(this.hotelDataSet1.NhanVien);
+            // TODO: This line of code loads data into the 'hotelDataSet.NhanVien' table. You can move, or remove it, as needed.
+           
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView1.CurrentRow.Selected = true;
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
